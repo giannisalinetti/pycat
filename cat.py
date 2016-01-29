3c868748b29b5bb3e3c5c237f28d1471579c32ef
Gianni Salinetti <gbsalinetti@gmail.com>
2016-01-29 21:40:34 +0100

    Function driven main

--- cat.py
+++ cat.py
@@ -1,36 +1,53 @@
 #!/usr/bin/env python
+"""This small utulity is a test program made during a train travel from Milan to Rome.
+   Its purpose is to show file contents using lists to load lines.
+"""
 
 import sys
 import re
 
+
 def usage():
+    """Prints program usage"""
     print("Usage: " + sys.argv[0] + " FILE")
 
-if len(sys.argv) == 1:
-    print(sys.argv[0] + ": missing file name argument")
-    usage()
-    sys.exit(1)
 
-if sys.argv[1] == '-h' or sys.argv[0] == '--help':
-    usage()
-    sys.exit(0)
+def argsCheck():
+    """Checks program arguments"""
+    if len(sys.argv) == 1:
+        print(sys.argv[0] + ": missing file name argument")
+        usage()
+        sys.exit(1)
 
-argFile = sys.argv[1]
+    if sys.argv[1] == '-h' or sys.argv[0] == '--help':
+        usage()
 
-# Open file (read mode) and load lines in the fileContainer list
-fileContainer = [] 
 
-content = open(argFile, 'r')
-while True:
-    line = content.readline()
-    if line != '':
-        fileContainer.append(line)    
-    else:
-        break
-content.close()
+def listLoad(argFile):
+    """Takes a file as an argument and returns a raw list of its lines"""    
+    fileContainer = [] 
+    content = open(argFile, 'r')
+    while True:
+        line = content.readline()
+        if line != '':
+            fileContainer.append(line)    
+        else:
+            break
+    content.close()
+    return fileContainer
 
-# Print cleaning up extra \n line endings
-for item in fileContainer:
-    print(re.sub('\\n', '', item))
 
-sys.exit(0)
+def main():
+    """Main function"""
+    argsCheck()
+    rawOutList = listLoad(sys.argv[1])
+
+    # Print cleaning up extra \n line endings
+    for item in rawOutList:
+        print(re.sub('\\n', '', item))
+    return(0)
+
+if __name__ == '__main__':
+    main()
+
+
